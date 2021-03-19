42ada914aa3011c7a5f748b4a2994210c42148ad
but-sergey <but-sergey@bk.ru>
2021-03-19 15:05:56 +0300

    Код для кнопки if/else

--- 2-Training/MainWindow.xaml.cs
+++ 2-Training/MainWindow.xaml.cs
@@ -39,7 +39,15 @@ namespace _2_Training
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
-
+            int x = 5;
+            if (x == 10)
+            {
+                myLabel.Text = "x must be 10";
+            }
+            else
+            {
+                myLabel.Text = "x isn't 10";
+            }
         }
 
         private void button3_Click(object sender, RoutedEventArgs e)
